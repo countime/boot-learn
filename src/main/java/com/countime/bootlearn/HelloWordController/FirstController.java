f5568a986ebb2b6fd1eea2593b882459846fdda1
zhaoyang <zhaoyang@anji-ceva.com>
2018-12-02 00:05:48 +0800

    理解spring的profile

--- src/main/java/com/countime/bootlearn/HelloWordController/FirstController.java
+++ src/main/java/com/countime/bootlearn/HelloWordController/FirstController.java
@@ -1,5 +1,6 @@
 package com.countime.bootlearn.HelloWordController;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,17 @@ public class FirstController {
     @RequestMapping("/")
     public String hello() {
         return "hello world";
+    }
+
+
+    @Value("${test.key1}")
+    private String testKey1;
+    @Value("${test.key2}")
+    private String testKey2;
+
+    @RequestMapping("/profiletest")
+    public String profiletest() {
+        return "key1:" + testKey1 + "</br>" +
+                "key2:" + testKey2;
     }
 }
