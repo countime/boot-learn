16c14cf304ca5943b911ccb6a00a2aedf40a4485
zhaoyang <zhaoyang@anji-ceva.com>
2018-12-01 23:19:30 +0800

    最简单的springboot项目

---
+++ src/main/java/com/countime/bootlearn/BootLearnApplication.java
@@ -0,0 +1,11 @@
+package com.countime.bootlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLearnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootLearnApplication.class, args);
+    }
+}
